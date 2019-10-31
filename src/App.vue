180b834468a45448dacec897a1c5f6e1f64c45a6
swimpenguin <swimpenguin0504@gmail.com>
2019-10-31 17:22:00 +0900

    [add] Add feature to add new task

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template lang="html">
   <div>
+    <!-- show todos -->
     <table>
       <thead>
         <tr>
@@ -9,6 +10,7 @@
           <th class="button">-</th>
         </tr>
       </thead>
+      <tbody>
         <tr v-for="item in $data.todos" v-bind:key="item.id">
           <th>{{ item.id }}</th>
           <td>{{ item.comment }}</td>
@@ -19,38 +21,41 @@
             <button>Delete</button>
           </td>
         </tr>
-      <tbody>
-
       </tbody>
     </table>
+
+    <!-- create new todo -->
+    <h2>Add new task</h2>
+    <form class="add-form" v-on:submit.prevent="doAdd">
+      Comment<input type="text" ref="comment">
+      <button type="submit">Add</button>
+    </form>
   </div>
 </template>
 
 <script>
-// use local storage
-const STORAGE_KEY = 'todos-vusjs-demo';
-const todoStorage = {
-  fetch() {
-    const todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    );
-    todos.forEach((todo, index) => {
-      todo.id = index;
-    });
-    todoStorage.uid = todos.length;
-  },
-  save(todos) {
-    localStorage.setItem(
-      STORAGE_KEY, JSON.stringfy(todos));
-  }
-}
-
 export default {
   name: 'App',
   data() {
     return {
       todos: []
     };
+  },
+  methods: {
+    // Add new task to to-do list
+    doAdd() {
+      const comment = this.$refs.comment;
+      if (!comment.value.length) {
+        return;
+      }
+      this.$data.todos.push({
+        id: this.$data.todos.length + 1,
+        comment: comment.value,
+        state: 0
+      });
+      // 入力値をリセット
+      comment.value = '';
+    }
   }
 }
 </script>
